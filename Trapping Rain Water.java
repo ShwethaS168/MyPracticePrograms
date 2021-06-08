76a26594be9b3221ee23288c81209e1fceb0347e
ShwethaS168 <67478446+ShwethaS168@users.noreply.github.com>
2021-06-08 23:53:33 +0530

    Update and rename Trapping Rain Water to Trapping Rain Water.java

---
+++ Trapping Rain Water.java
@@ -0,0 +1,41 @@
+class Solution{
+    
+    // arr: input array
+    // n: size of array
+    // Function to find the trapped water between the blocks.
+    static int trappingWater(int arr[], int n) { 
+        
+        int  max_left = arr[0];
+        int max_right= arr[n-1] ;
+        int left = 1;
+        int  right = n-2;
+        int tw = 0;
+         while (left<=right) {
+              if(max_left < max_right){
+                  if(arr[left] < max_left){
+                     tw   =  tw+ (max_left- arr[left]);
+                        left++;
+                  }
+                  else{
+                  max_left = arr[left];
+                  left++;
+                  }
+              }
+              else{
+              if(arr[right]<max_right){
+                    tw   =  tw+ (max_right- arr[right]);
+                   right--;
+                   }
+              
+              else{
+                  max_right = arr[right];
+                  right--;
+                  }
+         
+         }
+         
+       }   
+         return tw;
+    } 
+}
+
