2978b89d8bc2d44ba3d552da1049808229b9a0fa
Shwetha <shwethasumukam@gmail.com>
2021-06-26 23:02:28 +0530

    Smallest Divisible Number

---
+++ PractProgramProject/src/Work/Smallest_Divisible_Number.java
@@ -0,0 +1,24 @@
+package Work;
+
+public class Smallest_Divisible_Number {
+
+	public static long getSmallestDivNum(long n){
+        // code here
+        
+         
+        long ans =1;
+        for(long i= 1; i<=n; i++){
+        ans = (ans*i) / (gcd(ans,i) ) ;
+        }
+        return ans;
+        
+    }
+    
+    static long gcd(long a,long b){
+          if (a%b != 0){
+            return  gcd (b, a%b);
+          }
+          else return b;
+          
+    }
+}
